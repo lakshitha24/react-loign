d86c3d911523ac767df1f4fdba80c922dda62d0c
ljayaweera@kemana.com <ljayaweera@kemana.com>
2020-09-19 20:32:00 +0530

    add changes

--- src/HomePage/Component/SideBar/SideBar.js
+++ src/HomePage/Component/SideBar/SideBar.js
@@ -15,8 +15,8 @@ function SideBar({getBtnId}){
     return(
         <div className="side-bar">
             <ul>
-                <li onClick={setBtnID} className={0 == btnID ? 'active': ''} id='0'><FontAwesomeIcon icon={faHome}/>Home</li>
-                <li onClick={setBtnID} className={1 == btnID ? 'active': ''} id='1'><FontAwesomeIcon icon={faFileAlt}/>Create User</li>
+                <li onClick={setBtnID} className={0 === btnID ? 'active': ''} id='0'><FontAwesomeIcon icon={faHome}/>Home</li>
+                <li onClick={setBtnID} className={1 === btnID ? 'active': ''} id='1'><FontAwesomeIcon icon={faFileAlt}/>Create User</li>
                 <li><Link to="/login"><FontAwesomeIcon icon={faSignOutAlt}/>Logout</Link></li>
             </ul>
         </div>
